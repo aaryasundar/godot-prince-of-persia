b46a05e7bb4afd48793a5e5f6a66aae955a793df
Arya Somasundaran <arya@Senthuraans-MacBook-Air.local>
2026-05-10 03:28:56 +0530

    set the actions run,jump and create princess

--- prince-of-persia/scripts/enemy.gd
+++ prince-of-persia/scripts/enemy.gd
@@ -1,12 +1,31 @@
 extends Node2D
 
+const SPEED = 60
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var direction = 1
+
+@export var patrol: bool = false
+@export var face_left: bool = false
+
+@onready var ray_cast_right = $RayCast2DRight
+@onready var ray_cast_left = $RayCast2DLeft
+@onready var animated_sprite = $AnimatedSprite2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-  
+func _ready():
+	if not patrol:
+		animated_sprite.flip_h = face_left
+
+
+func _process(delta):
+	if not patrol:
+		return
+
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+		
+	if ray_cast_left.is_colliding():
+		direction =  1
+		animated_sprite.flip_h = false
+	position.x += direction * SPEED * delta
