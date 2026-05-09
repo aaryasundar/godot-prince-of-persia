a5db8b96aa9fb2bef0f3ba648a0cf00be0108ba2
Arya Somasundaran <arya@Senthuraans-MacBook-Air.local>
2026-05-10 03:41:59 +0530

    set the other actions too

--- prince-of-persia/scripts/prince.gd
+++ prince-of-persia/scripts/prince.gd
@@ -7,7 +7,23 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
+var is_dead = false
+
 func _physics_process(delta):
+	if is_dead:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
+		return
+
+	if Input.is_action_just_pressed("death"):
+		is_dead = true
+		animated_sprite.play("death")
+		velocity.x = 0
+		move_and_slide()
+		return
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -17,27 +33,28 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# Get the i nput directions : -1,0,1 
-	var direction = Input.get_axis("move_left", "move_right" )
-	# Flip the sprite
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	# Play animations
-	if is_on_floor():
+	if is_on_floor() and Input.is_action_pressed("slide"):
+		animated_sprite.play("slide")
+	elif Input.is_action_pressed("fight"):
+		animated_sprite.play("fight")
+	elif is_on_floor():
 		if direction == 0:
 			animated_sprite.play("Idle")
 		else:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	# Aapply movements
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
