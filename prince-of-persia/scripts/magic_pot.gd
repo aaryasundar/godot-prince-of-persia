7470ec52ee702cffffff2f024870df4f0676b516
Arya Somasundaran <arya@Senthuraans-MacBook-Air.local>
2026-05-11 13:29:30 +0530

    added the run soundtrack

--- prince-of-persia/scripts/magic_pot.gd
+++ prince-of-persia/scripts/magic_pot.gd
@@ -1,8 +1,21 @@
 extends Area2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var sprite = $AnimatedSprite2D
+@onready var collision_shape = $CollisionShape2D
+@onready var pickup_sound = $PickupSound
 
+var is_collected := false
+
+@warning_ignore("unused_parameter")
 func _on_body_entered(body):
+	if is_collected:
+		return
+
+	is_collected = true
 	print("+1 live")
-	animation_player.play("pickup" )
-	
+	sprite.visible = false
+	collision_shape.set_deferred("disabled", true)
+	pickup_sound.play()
+	await pickup_sound.finished
+	queue_free()
--- prince-of-persia/scripts/prince.gd
+++ prince-of-persia/scripts/prince.gd
@@ -6,11 +6,15 @@ const JUMP_VELOCITY = -450.0
 # Keep gravity typed and ensure non-zero fallback.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var run_sound = $RunSound
 
 var is_dead = false
 
 func _physics_process(delta):
 	if is_dead:
+		if run_sound.playing:
+			run_sound.stream_paused = false
+			run_sound.stop()
 		if not is_on_floor():
 			velocity.y += gravity * delta
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -19,6 +23,9 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("death"):
 		is_dead = true
+		if run_sound.playing:
+			run_sound.stream_paused = false
+			run_sound.stop()
 		animated_sprite.play("death")
 		velocity.x = 0
 		move_and_slide()
@@ -56,5 +63,15 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+
+	var has_move_input = Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")
+	var is_running = is_on_floor() and has_move_input and not Input.is_action_pressed("slide") and not Input.is_action_pressed("fight")
+	if is_running:
+		if run_sound.playing:
+			run_sound.stream_paused = false
+		else:
+			run_sound.play()
+	elif run_sound.playing:
+		run_sound.stream_paused = true
 
 	move_and_slide()
