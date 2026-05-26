3d636a59e6a90b5e4976a73504bf02509bf529b7
Arya Somasundaran <arya@Senthuraans-MacBook-Air.local>
2026-05-27 00:51:21 +0530

    set the tiles for level2

--- prince-of-persia/scripts/lives_hud.gd
+++ prince-of-persia/scripts/lives_hud.gd
@@ -1,15 +1,9 @@
 extends CanvasLayer
 
 const HEART := "♥"
-const BANNER_HOLD_SEC := 1.2
-const BANNER_FADE_SEC := 0.5
-
 @onready var lives_label: Label = $MarginContainer/LivesLabel
 @onready var level_label: Label = $LevelMargin/LevelLabel
 @onready var level_banner: Control = $LevelBanner
-@onready var level_number_label: Label = $LevelBanner/LevelNumber
-
-var _banner_tween: Tween
 
 
 func _ready() -> void:
@@ -35,17 +29,3 @@ func _on_lives_changed(current: int) -> void:
 func show_level(number: int) -> void:
 	var n := maxi(number, 1)
 	level_label.text = "Level %d" % n
-	level_number_label.text = str(n)
-	level_banner.visible = true
-	level_banner.modulate = Color(1, 1, 1, 1)
-	if _banner_tween:
-		_banner_tween.kill()
-	_banner_tween = create_tween()
-	_banner_tween.tween_interval(BANNER_HOLD_SEC)
-	_banner_tween.tween_property(level_banner, "modulate:a", 0.0, BANNER_FADE_SEC)
-	_banner_tween.tween_callback(_hide_level_banner)
-
-
-func _hide_level_banner() -> void:
-	level_banner.visible = false
-	level_banner.modulate = Color(1, 1, 1, 1)
